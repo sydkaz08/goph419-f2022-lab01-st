40cc7155434a6de60bc9755de27815f28582a187
Ali Kazmi <hur.kazmi1@ucalgary.ca>
2022-10-07 22:18:39 -0600

    second commit

--- source/main.py
+++ source/main.py
@@ -14,20 +14,17 @@ def partial_derivative(alpha,ve_v0):
         (-alpha/(1+alpha)*2*ve_v0)
     return pdtalpha, pdtve_v0
 
-
-
-    
 # launch_exact calculates the launch angle for max_alpha and min_alpha
 # takes user defined alpha, tol_alpha, and ve_v0
 # This computation is done using the built in sin function
 
-def launch_angles1(alpha, tol_alpha, ve_v0):  #uses the math.asin function to test for error
+def launch_exact(alpha, tol_alpha, ve_v0): #uses the math.asin function to test for error
     min_alpha = (1 - tol_alpha) * alpha
     max_alpha = (1 + tol_alpha) * alpha
-    math_temp=1-((min_alpha/(1+min_alpha)*ve_v0**2))
-    if math_temp>0:
+    in_rt=1-((min_alpha/(1+min_alpha)*ve_v0**2))
+    if in_rt>0:
         try:
-            sin_phi=(1+min_alpha)*math.sqrt(math_temp)
+            sin_phi=(1+min_alpha)*math.sqrt(in_rt)
             max_angle=math.asin(sin_phi)
             if max_angle>math.pi/2 or max_angle<0:
                 max_angle=None
@@ -35,10 +32,10 @@ def launch_angles1(alpha, tol_alpha, ve_v0):  #uses the math.asin function to te
             max_angle=None
     else:
         max_angle=None
-    math_temp=1-((max_alpha/(1+max_alpha)*ve_v0**2))
-    if math_temp>0:
+    in_rt=1-((max_alpha/(1+max_alpha)*ve_v0**2))
+    if in_rt>0:
         try:
-            sin_phi=(1+max_alpha)*math.sqrt(math_temp)
+            sin_phi=(1+max_alpha)*math.sqrt(in_rt)
             min_angle=math.asin(sin_phi)
             if min_angle>math.pi/2 or min_angle<0:
                 min_angle=None
@@ -48,9 +45,7 @@ def launch_angles1(alpha, tol_alpha, ve_v0):  #uses the math.asin function to te
         min_angle=None
     return min_angle, max_angle
 
-
-
-def launch_angles2(alpha, tol_alpha, ve_v0): #using the arcsin() function created using Bowein and Chamberland
+def launch_approx(alpha, tol_alpha, ve_v0): #using the arcsin() function created using Bowein and Chamberland
     min_alpha = (1 - tol_alpha) * alpha
     max_alpha = (1 + tol_alpha) * alpha
     math_temp=1-((min_alpha/(1+min_alpha)*ve_v0**2))
@@ -71,43 +66,36 @@ def launch_angles2(alpha, tol_alpha, ve_v0): #using the arcsin() function create
         min_angle=None
     return max_angle, min_angle
 
-
 #To check if the error between the exact and approximate values
 #Takes values calculates in launch_angles as x_true and x_approx
 
-def partb(x_true,x_approx):
-    true_error=abs(x_true-x_approx)
-    true_relative_error=true_error/x_true
+def error(la_true,la_approx):
+    true_error=abs(la_true-la_approx)
+    true_relative_error=true_error/la_true
     print("True Error:",true_error,"\nTrue Relative Error:",true_relative_error)
     
-    
 #This function plotts the min and max launch angles over a rangle of alpha values
-
-def partc():
+def fig1():
     ve_v0=2
     tol_alpha=0.04
-    n_alpha=int(0.5//0.001+1) #number of alpha 
-    alpha=np.linspace(0,0.5,n_alpha)
-    min_angle=[0]*n_alpha
-    max_angle=[0]*n_alpha
-    for i in range(n_alpha):
-        max_angle[i],min_angle[i]=launch_angles2(alpha[i], tol_alpha, ve_v0)
-    plt.plot(alpha,min_angle, label = 'Minimum Alpha')
-    plt.plot(alpha,max_angle, label = 'Maximum Alpha')
+    num_alpha=int(0.5//0.001+1) #number of alpha 
+    alpha=np.linspace(0,0.5,num_alpha)
+    min_angle=[0]*num_alpha
+    max_angle=[0]*num_alpha
+    for i in range(num_alpha):
+        max_angle[i],min_angle[i]=launch_approx(alpha[i], tol_alpha, ve_v0)
+    plt.plot(alpha,min_angle, label ='Minimum Alpha')
+    plt.plot(alpha,max_angle, label ='Maximum Alpha')
     plt.xlabel(chr(945))
     plt.ylabel(chr(966))
     plt.title("Minimum and Maximum Launch Angles with Fluctuating "+str(chr(945))+"\n")
     plt.legend()
     plt.savefig('/users/alikazmi/Desktop/GOPH/goph419-f2022-lab01-st/source/Figures/Figure(1)')
     plt.show()
-
-
-
     
-
 #This function answers part d, plotting the min and max launch angles over a
 #range of ve_v0 values
-def partd():
+def fig2():
     alpha=0.25
     tol_alpha=0.04
     n_ve_v0=int(2.5//0.001+1)
@@ -115,7 +103,7 @@ def partd():
     min_angle=[0]*n_ve_v0
     max_angle=[0]*n_ve_v0
     for i in range (n_ve_v0):
-        max_angle[i],min_angle[i]=launch_angles2(alpha, tol_alpha, ve_v0[i])
+        max_angle[i],min_angle[i]=launch_approx(alpha, tol_alpha, ve_v0[i])
     plt.plot(ve_v0,min_angle, label = 'Minimum Alpha')
     plt.plot(ve_v0,max_angle, label ='Maximum Alpha')
     plt.xlabel("ve/v0")
@@ -125,42 +113,41 @@ def partd():
     plt.savefig('/users/alikazmi/Desktop/GOPH/goph419-f2022-lab01-st/source/Figures/Figure(2)')
     plt.show()
     
-    
 #This function computes the error in sin_phi, and condition number
 
-def parte():
-    # page 48 eq: 4.27
+#This function computes the error in sin_phi, and condition number
+def uncertainty():
     ve_v0=2 
     alpha=0.25
     delta_ve_v0=0.05
     delta_alpha=0.02
-    math_temp=1-((alpha/(1+alpha))*(ve_v0**2))
-    sin_phi=(1+alpha)*math.sqrt(math_temp)
-    pd_alpha,pd_ve_v0=partial_derivative(alpha,ve_v0)
-    delta_sin_phi=abs(pd_alpha)*delta_alpha+abs(pd_ve_v0)*delta_ve_v0
-    print("\nError of sin(" + str(chr(966)) + "):",str(delta_sin_phi))
-    # Note the following equation for CN from Q7,8,9 from Quiz 1
+    in_rt=1-((alpha/(1+alpha))*(ve_v0**2))
+    sin_phi=(1+alpha)*math.sqrt(in_rt)
+    pdtalpha,pdtve_v0=partial_derivative(alpha,ve_v0)
+    deltasin_phi=abs(pdtalpha)*delta_alpha+abs(pdtve_v0)*delta_ve_v0
+    print("\nError of sin(" + str(chr(966)) + "):",str(deltasin_phi))
+    
+
+    # From the Notes
     x=math.sqrt(ve_v0**2+alpha**2) #Norm of input values
     f=sin_phi #Function Result
-    J=math.sqrt(pd_alpha**2+pd_ve_v0**2) #Jacobian Matrix
+    J=math.sqrt(pdtalpha**2+pdtve_v0**2) #Jacobian Matrix
     CN=x*J/f #solve for CN
     print("\nCondition Number:",CN)
 
-    
-    
 # start() calls previous functions
 def start():
-    max_angle2,min_angle2=launch_angles2(0.25, 0.02, 2)  #APPROX when alpha=0.25, ve_v0=2, alpha_tol=2%
+    max_angle2,min_angle2=launch_approx(0.25, 0.02, 2)  #APPROX when alpha=0.25, ve_v0=2, alpha_tol=2%
     print("Minumum Angle (APPROX):",str(min_angle2)+"\nMaximum Angle (APPROX):",max_angle2)
-    min_angle1,max_angle1=launch_angles1(0.25, 0.02, 2)  #TRUE when alpha=0.25, ve_v0=2, alpha_tol=2%
+    min_angle1,max_angle1=launch_exact(0.25, 0.02, 2)  #TRUE when alpha=0.25, ve_v0=2, alpha_tol=2%
     print("Minumum Angle (EXACT): "+str(min_angle1)+"\nMaximum Angle (EXACT): "+ str(max_angle1)+"\n")
     print("For min_angle")
-    partb(min_angle1,min_angle2)
+    error(min_angle1,min_angle2)
     print("\nFor max angle")
     
-    partb(max_angle1,max_angle2)
-    partc()
-    partd()
-    parte()
+    error(max_angle1,max_angle2)
+    fig1()
+    fig2()
+    uncertainty()
     
 start()
